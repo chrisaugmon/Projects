433d9154763c7b6acb7277a6109b11943056cae2
Christopher Branner-Augmon <chrisaugmon@berkeley.edu>
2016-01-24 12:46:01 -0800

    Molecular_Weight.py

---
+++ Molecular_Weight.py
@@ -0,0 +1,57 @@
+
+abbreviations = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 
+'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 
+'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 
+'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 
+'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 
+'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 
+'Sg', 'Bh', 'Hs', 'Mt']
+
+weights = [1.0079, 4.0026, 6.941, 9.0122, 10.811, 12.0107, 14.0067, 15.9994, 18.9984, 20.1797, 22.9897, 
+24.305, 26.9815, 28.0855, 30.9738, 32.065, 35.453, 39.948, 39.0983, 40.078, 44.9559, 47.867, 50.9415, 
+51.9961, 54.938, 55.845, 58.9332, 58.6934, 63.546, 65.39, 69.723, 72.64, 74.9216, 78.96, 79.904, 83.8, 
+85.4678, 87.62, 88.9059, 91.224, 92.9064, 95.94, 98, 101.07, 102.9055, 106.42, 107.8682, 112.411, 114.818, 
+118.71, 121.76, 127.6, 126.9045, 131.293, 132.9055, 137.327, 138.9055, 140.116, 140.9077, 144.24, 145, 150.36, 
+151.964, 157.25, 158.9253, 162.5, 164.9303, 167.259, 168.9342, 173.04, 174.967, 178.49, 180.9479, 183.84, 186.207, 
+190.23, 192.217, 195.078, 196.9665, 200.59, 204.3833, 207.2, 208.9804, 209, 210, 222, 223, 226, 227, 232.0381, 231.0359, 
+238.0289, 237, 244, 243, 247, 247, 251, 252, 257, 258, 259, 262, 261, 262, 266, 264, 277, 268]
+
+dictionary = {x:y for x,y in zip(abbreviations, weights)}
+
+class invalid_formula(Exception):
+	pass
+
+def weight(formula):
+	elements = []
+	curr_elem = ''
+	subscript = ''
+	for x in formula:
+		if x.isdigit() and curr_elem:
+			subscript += x
+		elif x.isdigit() and not curr_elem:
+			raise invalid_formula("Number not associated with an element")
+		elif x.isupper() and curr_elem and subscript:
+			for y in range(int(subscript)):
+				elements.append(curr_elem)
+			curr_elem = x
+			subscript = ''
+		elif x.isupper() and curr_elem and not subscript:
+			elements.append(curr_elem)
+			curr_elem = x
+			subscript = ''
+		else:
+			curr_elem += x
+	if subscript:
+		for z in range(int(subscript)):
+			elements.append(curr_elem)
+	else:
+		elements.append(curr_elem)
+	for x in elements:
+		if x not in abbreviations:
+			raise invalid_formula("One or more elements invalid")
+	total = 0
+	for x in elements:
+		total += dictionary[x]
+	return total
+
+
